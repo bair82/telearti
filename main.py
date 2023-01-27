94de74d1b3b4833c068c5300cd5b455e38380d8e
bair82 <60368178+bair82@users.noreply.github.com>
2023-01-27 18:17:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import telebot
 from flask import Flask, request
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
+
+URL = "https://telearti.onrender.com/"
 
 bot = telebot.TeleBot(BOT_TOKEN, threaded=False)
 app = Flask(__name__)
