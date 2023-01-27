c5a31c866f18000449120e33c1f2d5536db31bdd
bair82 <60368178+bair82@users.noreply.github.com>
2023-01-27 18:07:51 +0100

    Update telearti.py

--- telearti.py
+++ telearti.py
@@ -1,9 +1,11 @@
 import os
 import telebot
+from flask import Flask, request
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN, threaded=False)
+app = Flask(__name__)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
@@ -12,3 +14,22 @@ def send_welcome(message):
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
+
+# Webhook
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=URL + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
