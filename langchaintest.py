779d5aab58434d646ea973d24cb5138a4b15c262
bair82 <60368178+bair82@users.noreply.github.com>
2023-01-27 22:19:15 +0100

    Create langchaintest.py

---
+++ langchaintest.py
@@ -0,0 +1,19 @@
+import langchain
+
+from langchain.llms import Cohere, OpenAI
+langchain.verbose=True
+from langchain.chains.conversation.memory import ConversationSummaryBufferMemory
+
+stop_words = ["AI:","Human:"]
+llm = Cohere(model="command-xlarge-nightly",temperature=0, model_kwargs={'stop': stop_words})
+
+conversation_with_summary = langchain.ConversationChain(
+    llm=llm, 
+    # We set a very low max_token_limit for the purposes of testing.
+    memory=ConversationSummaryBufferMemory(llm=llm, max_token_limit=100),
+    verbose=True
+)
+
+def reply(user_input):
+  response = conversation_with_summary.predict(input=user_input)
+  return response
